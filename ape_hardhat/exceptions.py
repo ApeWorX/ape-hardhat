56d0379f404ad72b0d37fbf5a91758cb0a047f66
unparalleled-js <yingthi@live.com>
2022-01-22 15:26:48 -0600

    refactor: just close process in exit

--- ape_hardhat/exceptions.py
+++ ape_hardhat/exceptions.py
@@ -48,6 +48,7 @@ class HardhatTimeoutError(HardhatSubprocessError):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cancel()
         return False
 
     def __str__(self):
@@ -59,27 +60,27 @@ class HardhatTimeoutError(HardhatSubprocessError):
     @property
     def expire_at(self) -> float:
         if self._seconds is None:
-            raise self._error(
-                ValueError("Timeouts with 'seconds == None' do not have an expiration time.")
+            raise ValueError(
+                "Timeouts with 'seconds == None' do not have an expiration time."
             )
         elif self._start_time is None:
-            raise self._error(ValueError("Timeout has not been started."))
+            raise ValueError("Timeout has not been started.")
 
         return self._start_time + self._seconds
 
     def start(self):
         if self._is_running is not None:
-            raise self._error(ValueError("Timeout has already been started."))
+            raise ValueError("Timeout has already been started.")
 
         self._start_time = time.time()
         self._is_running = True
 
     def check(self):
         if self._is_running is None:
-            raise self._error(ValueError("Timeout has not been started."))
+            raise ValueError("Timeout has not been started.")
 
         elif self._is_running is False:
-            raise self._error(ValueError("Timeout has already been cancelled."))
+            raise ValueError("Timeout has already been cancelled.")
 
         elif self._seconds is None:
             return
@@ -88,19 +89,16 @@ class HardhatTimeoutError(HardhatSubprocessError):
             self._is_running = False
 
             if isinstance(self._exception, type):
-                raise self._error(self._exception(str(self)))
+                raise self._exception(str(self))
 
             elif isinstance(self._exception, Exception):
-                raise self._error(self._exception)
+                raise self._exception
 
             raise self
 
     def cancel(self):
-        self._is_running = False
-
-    def _error(self, err: Exception) -> Exception:
         self._process.stop()
-        return err
+        self._is_running = False
 
 
 class RPCTimeoutError(HardhatTimeoutError):
