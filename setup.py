cf5ec3bd27e59245bbf8480fc6505267b1521dd5
unparalleled-js <yingthi@live.com>
2022-03-11 15:27:17 -0600

    chore: correctly bump ape

--- setup.py
+++ setup.py
@@ -67,10 +67,10 @@ setup(
     url="https://github.com/ApeWorX/ape-hardhat",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0",
+        "eth-ape>=0.1.0,<0.2.0",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
-    python_requires=">=3.7,<4",
+    python_requires=">=3.7.2,<4",
     extras_require=extras_require,
     py_modules=["ape_hardhat"],
     license="Apache-2.0",
