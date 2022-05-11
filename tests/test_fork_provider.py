575af9ba609211d751bd7d6fdd675285d4763d6d
unparalleled-js <yingthi@live.com>
2022-05-11 16:34:26 -0500

    fix: help

--- tests/test_fork_provider.py
+++ tests/test_fork_provider.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 from ape.contracts import ContractContainer, ContractInstance
+from ape.exceptions import ContractLogicError
 from ethpm_types import ContractType
 
 from ape_hardhat.providers import HardhatMainnetForkProvider
@@ -11,14 +12,16 @@ TESTS_DIRECTORY = Path(__file__).parent
 alchemy_xfail = pytest.mark.xfail(strict=False, reason="Fails to establish connection with Alchemy")
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def mainnet_fork_provider(networks):
     network_api = networks.ecosystems["ethereum"]["mainnet-fork"]
     provider = create_mainnet_fork_provider(network_api)
     provider.port = 9001
     provider.connect()
+    networks.active_provider = provider
     yield provider
     provider.disconnect()
+    networks.active_provider = None
 
 
 def create_mainnet_fork_provider(network_api):
@@ -29,6 +32,34 @@ def create_mainnet_fork_provider(network_api):
         data_folder=Path("."),
         provider_settings={},
     )
+
+
+@pytest.fixture(scope="module")
+def contract_type(raw_contract_type) -> ContractType:
+    return ContractType.parse_obj(raw_contract_type)
+
+
+@pytest.fixture(scope="module")
+def contract_container(contract_type) -> ContractContainer:
+    return ContractContainer(contract_type=contract_type)
+
+
+@pytest.fixture(scope="module")
+def contract_instance(owner, contract_container, mainnet_fork_provider) -> ContractInstance:
+    return owner.deploy(contract_container)
+
+
+def create_fork_provider(network_api, port):
+    provider = HardhatMainnetForkProvider(
+        name="hardhat",
+        network=network_api,
+        request_header={},
+        data_folder=Path("."),
+        provider_settings={},
+    )
+    provider.port = port
+    return provider
+
 
 @alchemy_xfail
 def test_request_timeout(mainnet_fork_provider, config, network_api):
@@ -44,33 +75,6 @@ def test_request_timeout(mainnet_fork_provider, config, network_api):
             assert provider.timeout == 300
 
 
-@pytest.fixture(scope="module")
-def contract_type(raw_contract_type) -> ContractType:
-    return ContractType.parse_obj(raw_contract_type)
-
-
-@pytest.fixture(scope="module")
-def contract_container(contract_type) -> ContractContainer:
-    return ContractContainer(contract_type=contract_type)
-
-
-@pytest.fixture()
-def contract_instance(owner, contract_container, mainnet_fork_provider) -> ContractInstance:
-    return owner.deploy(contract_container)
-
-@alchemy_xfail
-def create_fork_provider(network_api, port):
-    provider = HardhatMainnetForkProvider(
-        name="hardhat",
-        network=network_api,
-        request_header={},
-        data_folder=Path("."),
-        provider_settings={},
-    )
-    provider.port = port
-    return provider
-
-
 @alchemy_xfail
 def test_reset_fork(networks, mainnet_fork_provider):
     mainnet_fork_provider.mine()
@@ -80,10 +84,8 @@ def test_reset_fork(networks, mainnet_fork_provider):
     assert block_num_after_reset < prev_block_num
 
 
-def test_transaction_contract_as_sender(network_api, contract_instance, provider):
-    provider = get_hardhat_provider(network_api)
-    provider.connect()
-    networks.active_provider = provider
-    assert contract_instance
-    # contract_instance.set_number(10, sender=contract_instance)
-    # assert contract_instance.my_number() == 10
+@alchemy_xfail
+def test_transaction_contract_as_sender(networks, network_api, contract_instance):
+    with pytest.raises(ContractLogicError):
+        # Task failed successfully
+        contract_instance.set_number(10, sender=contract_instance)
