4939ace657465eaf0a8ad5ec71121e94946567e2
Juliya Smith <yingthi@live.com>
2022-06-10 08:22:59 -0500

    chore: update ape and refactor tests (#67)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,13 @@
-import json
 from pathlib import Path
 
 import ape
 import pytest  # type: ignore
 from _pytest.runner import pytest_runtest_makereport as orig_pytest_runtest_makereport
-from ape.api.networks import LOCAL_NETWORK_NAME, NetworkAPI
+from ape.api.networks import LOCAL_NETWORK_NAME
 from ape.contracts import ContractContainer
-from ape.managers.project import ProjectManager
 from ethpm_types import ContractType
 
 from ape_hardhat import HardhatProvider
-
-
-def get_project() -> ProjectManager:
-    return ape.Project(Path(__file__).parent)
-
-
-def get_hardhat_provider(network_api: NetworkAPI):
-    return HardhatProvider(
-        name="hardhat",
-        network=network_api,
-        request_header={},
-        data_folder=Path("."),
-        provider_settings={},
-    )
-
 
 BASE_CONTRACTS_PATH = Path(__file__).parent / "data" / "contracts"
 
@@ -38,15 +21,50 @@ def pytest_runtest_makereport(item, call):
     return tr
 
 
+@pytest.fixture(scope="session", autouse=True)
+def in_tests_dir(config):
+    with config.using_project(Path(__file__).parent):
+        yield
+
+
+@pytest.fixture(scope="session")
+def config():
+    return ape.config
+
+
+@pytest.fixture(scope="session")
+def accounts():
+    return ape.accounts.test_accounts
+
+
+@pytest.fixture(scope="session")
+def networks():
+    return ape.networks
+
+
+@pytest.fixture(scope="session")
+def get_hardhat_provider(local_network_api):
+    def method():
+        return HardhatProvider(
+            name="hardhat",
+            network=local_network_api,
+            request_header={},
+            data_folder=Path("."),
+            provider_settings={},
+        )
+
+    return method
+
+
 @pytest.fixture(scope="session", params=("solidity", "vyper"))
 def raw_contract_type(request):
     path = BASE_CONTRACTS_PATH / f"{request.param}_contract.json"
-    return json.loads(path.read_text())
+    return path.read_text()
 
 
 @pytest.fixture(scope="session")
 def contract_type(raw_contract_type) -> ContractType:
-    return ContractType.parse_obj(raw_contract_type)
+    return ContractType.parse_raw(raw_contract_type)
 
 
 @pytest.fixture(scope="session")
@@ -57,22 +75,6 @@ def contract_container(contract_type) -> ContractContainer:
 @pytest.fixture(scope="session")
 def contract_instance(owner, contract_container, hardhat_connected):
     return owner.deploy(contract_container)
-
-
-@pytest.fixture(scope="session")
-def config():
-    return ape.config
-
-
-@pytest.fixture(scope="session", autouse=True)
-def in_tests_dir(config):
-    with config.using_project(Path(__file__).parent):
-        yield
-
-
-@pytest.fixture(scope="session")
-def accounts():
-    return ape.accounts.test_accounts
 
 
 @pytest.fixture(scope="session")
@@ -91,32 +93,16 @@ def owner(accounts):
 
 
 @pytest.fixture(scope="session")
-def project():
-    return get_project()
-
-
-@pytest.fixture(scope="session")
-def networks():
-    return ape.networks
-
-
-@pytest.fixture(scope="session")
 def local_network_api(networks):
     return networks.ecosystems["ethereum"][LOCAL_NETWORK_NAME]
 
 
 @pytest.fixture(scope="session")
-def hardhat_disconnected(local_network_api):
-    provider = get_hardhat_provider(local_network_api)
-    return provider
+def hardhat_disconnected(get_hardhat_provider):
+    return get_hardhat_provider()
 
 
 @pytest.fixture(scope="session")
-def hardhat_connected(networks, local_network_api):
-    provider = get_hardhat_provider(local_network_api)
-    provider.connect()
-    original_provider = networks.active_provider
-    networks.active_provider = provider
-    yield provider
-    provider.disconnect()
-    networks.active_provider = original_provider
+def hardhat_connected(networks):
+    with networks.parse_network_choice("ethereum:local:hardhat") as provider:
+        yield provider
