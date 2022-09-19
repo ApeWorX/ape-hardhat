34210a540a955d0a5fd6e0401b387f0338731146
unparalleled-js <yingthi@live.com>
2022-09-19 16:38:31 -0500

    test: wip

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,11 +1,12 @@
 import shutil
 from pathlib import Path
+from typing import List
 
 import pytest
 from ape.contracts import ContractContainer
 from ethpm_types import ContractType
 
-from .expected_traces import LOCAL_TRACE, MAINNET_FAIL_TRACE, MAINNET_TRACE
+from .expected_traces import LOCAL_TRACE, MAINNET_FAIL_TRACE, MAINNET_TRACE, LOCAL_GAS_REPORT
 
 MAINNET_FAIL_TXN_HASH = "0x053cba5c12172654d894f66d5670bab6215517a94189a9ffc09bc40a589ec04d"
 MAINNET_TXN_HASH = "0xb7d7f1d5ce7743e821d3026647df486f517946ef1342a1ae93c96e4a8016eab7"
@@ -54,7 +55,7 @@ def local_receipt(contract_a, owner):
 @pytest.fixture
 def trace_capture(capsys):
     def get():
-        output, _ = capsys.readouterr()
+        output, errput = capsys.readouterr()
         return [s.strip() for s in output.split("\n")]
 
     return get
@@ -62,14 +63,39 @@ def trace_capture(capsys):
 
 def test_local_transaction_traces(local_receipt, trace_capture):
     local_receipt.show_trace()
-    assert all([x in LOCAL_TRACE for x in trace_capture()])
+    raise ValueError(trace_capture())
+    assert_rich_output(trace_capture(), LOCAL_TRACE)
 
     # Verify can happen more than once.
     local_receipt.show_trace()
-    assert all([x in LOCAL_TRACE for x in trace_capture()])
+    assert_rich_output(trace_capture(), LOCAL_TRACE)
+
+
+def test_local_transaction_gas_report(local_receipt, trace_capture):
+    local_receipt.show_gas_report()
+    assert_rich_output(trace_capture(), LOCAL_GAS_REPORT)
+
+    # Verify can happen more than once.
+    local_receipt.show_gas_report()
+    assert_rich_output(trace_capture(), LOCAL_GAS_REPORT)
 
 
 @pytest.mark.manual
 def test_mainnet_transaction_traces(mainnet_receipt, trace_capture):
     mainnet_receipt.show_trace()
-    assert all([x in EXPECTED_MAP[mainnet_receipt.txn_hash] for x in trace_capture()])
+    assert_rich_output(trace_capture(), EXPECTED_MAP[mainnet_receipt.txn_hash])
+
+
+def assert_rich_output(rich_capture: List[str], expected: str):
+    expected_lines = [x.strip() for x in expected.split("\n") if x.strip()]
+    actual_lines = [x.strip() for x in rich_capture if x.strip()]
+    assert actual_lines, "No output."
+
+    for actual, expected in zip(actual_lines, expected_lines):
+        assert actual == expected
+
+    actual_len = len(actual_lines)
+    expected_len = len(expected_lines)
+    if expected_len > actual_len:
+        rest = "\n".join(expected_lines[actual_len:])
+        pytest.fail(f"Missing expected lines: {rest}")
