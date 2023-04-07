37e4a7660b967b77b97d6008204972c7a5965a36
Semi Invader <41244007+jmanywhere@users.noreply.github.com>
2023-04-07 12:54:38 -0600

    Added bsc plugin as another possible fork network
    Needed bsc added to the list, this change came from the recommendation of fubuloubu in Discord.

--- ape_hardhat/__init__.py
+++ ape_hardhat/__init__.py
@@ -38,7 +38,10 @@ def providers():
     yield "optimism", LOCAL_NETWORK_NAME, HardhatProvider
     yield "optimism", "mainnet-fork", HardhatForkProvider
     yield "optimism", "goerli-fork", HardhatForkProvider
-
+    
+    yield "bsc", LOCAL_NETWORK_NAME, HardhatProvider
+    yield "bsc", "mainnet-fork", HardhatProvider
+    yield "bsc", "testnet-fork", HardhatProvider
 
 __all__ = [
     "HardhatNetworkConfig",
